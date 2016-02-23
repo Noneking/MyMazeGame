f705e14d27c7f81fda0415d7b778c7227700f305
Noneking <aanoriega.mobile@gmail.com>
2016-02-23 20:20:07 +0100

    Initial Commit

--- app/src/main/java/com/example/nonek/mymazegame/Maze.java
+++ app/src/main/java/com/example/nonek/mymazegame/Maze.java
@@ -1,102 +1,98 @@
 package com.example.nonek.mymazegame;
 
+import java.io.Serializable;
+
 /**
  * Created by nonek on 18/02/2016.
  */
-public class Maze {
+public class Maze implements Serializable {
 
-    private final int UP=0, DOWN=1, RIGHT=2, LEFT=3;//Each number represent a direction
+    private static final long serialVersionUID = 1L;
+
+    public static final int UP = 0, DOWN = 1, RIGHT = 2, LEFT = 3;
 
     private boolean[][] verticalLines;
     private boolean[][] horizontalLines;
     private int sizeX, sizeY;         //stores the width and height of the maze
     private int currentX, currentY;   //stores the current location of the ball
     private int finalX, finalY;       //stores the finishing of the maze
+    private boolean gameComplete;
 
-    private boolean gameFinished=false;
-
-    public boolean canMove(int direction){
-        boolean movement=false;
-        switch(direction){
-            case UP:
-                if(currentY!=0 && !horizontalLines[currentY-1][currentX]){
-                    currentY--;
-                    movement=true;
-                }
-                break;
-            case DOWN:
-                if(currentY!=sizeY-1 && !horizontalLines[currentY][currentX]){
-                    currentY++;
-                    movement=true;
-                }
-                break;
-            case RIGHT:
-                if(currentX!=sizeX-1 && verticalLines[currentY][currentX]){
-                    currentX++;
-                    movement=true;
-                }
-                break;
-            case LEFT:
-                if(currentX!=0 && verticalLines[currentY][currentX-1]){
-                    currentX--;
-                    movement=true;
-                }
-                break;
-        }
-
-        if(currentY==finalY && currentX==finalX){
-            gameFinished=true;
-        }
-
-        return movement;
+    public int getMazeWidth() {
+        return sizeX;
     }
-
+    public int getMazeHeight() {
+        return sizeY;
+    }
+    public boolean canMove(int direction) {
+        boolean moved = false;
+        if(direction == UP) {
+            if(currentY != 0 && !horizontalLines[currentY-1][currentX]) {
+                currentY--;
+                moved = true;
+            }
+        }
+        if(direction == DOWN) {
+            if(currentY != sizeY-1 && !horizontalLines[currentY][currentX]) {
+                currentY++;
+                moved = true;
+            }
+        }
+        if(direction == RIGHT) {
+            if(currentX != sizeX-1 && !verticalLines[currentY][currentX]) {
+                currentX++;
+                moved = true;
+            }
+        }
+        if(direction == LEFT) {
+            if(currentX != 0 && !verticalLines[currentY][currentX-1]) {
+                currentX--;
+                moved = true;
+            }
+        }
+        if(moved) {
+            if(currentX == finalX && currentY == finalY) {
+                gameComplete = true;
+            }
+        }
+        return moved;
+    }
+    public boolean isGameComplete() {
+        return gameComplete;
+    }
     public void setStartPosition(int x, int y) {
         currentX = x;
         currentY = y;
     }
-
-    public int getCurrentX() {
-        return currentX;
-    }
-
-    public int getCurrentY() {
-        return currentY;
-    }
-
-    public boolean[][] getVerticalLines() {
-        return verticalLines;
-    }
-
-    public boolean[][] getHorizontalLines() {
-        return horizontalLines;
-    }
-
-    public void setHorizontalLines(boolean[][] lines) {
-        horizontalLines = lines;
-        sizeX = horizontalLines[0].length;
-    }
-
-    public void setVerticalLines(boolean[][] lines) {
-        verticalLines = lines;
-        sizeY = verticalLines.length;
-    }
-
     public int getFinalX() {
         return finalX;
     }
-
     public int getFinalY() {
         return finalY;
     }
-
     public void setFinalPosition(int x, int y) {
         finalX = x;
         finalY = y;
     }
-
-    public boolean isGameComplete() {
-        return gameFinished;
+    public int getCurrentX() {
+        return currentX;
+    }
+    public int getCurrentY() {
+        return currentY;
+    }
+    public boolean[][] getHorizontalLines() {
+        return horizontalLines;
+    }
+    public void setHorizontalLines(boolean[][] lines) {
+        horizontalLines = lines;
+        sizeX = horizontalLines[0].length;
+    }
+    public boolean[][] getVerticalLines() {
+        return verticalLines;
+    }
+    public void setVerticalLines(boolean[][] lines) {
+        verticalLines = lines;
+        sizeY = verticalLines.length;
     }
 
 }
--- app/src/main/java/com/example/nonek/mymazegame/MazeMaker.java
+++ app/src/main/java/com/example/nonek/mymazegame/MazeMaker.java
@@ -1,15 +1,18 @@
 package com.example.nonek.mymazegame;
 
+import android.util.Log;
+
 /**
  * Created by nonek on 18/02/2016.
  */
 public class MazeMaker {
 
-    private Maze maze;
-
-    public Maze getMaze(int mazeNum){
+    public static Maze getMaze(int mazeNum){
+        Maze maze=null;
+        Log.i("tab1","Entra dentro del metodo");
         switch(mazeNum){
             case 1:
+                Log.i("tab2","Entra dentro del switch 1");
                 maze = new Maze();
                 boolean[][] vLines = new boolean[][]{
                         {true ,false,false,false,true ,false,false},
@@ -43,6 +46,35 @@ public class MazeMaker {
                 break;
         }
         return maze;
+//        Maze maze = null;
+//        if(mazeNum == 1) {
+//            maze = new Maze();
+//            boolean[][] vLines = new boolean[][]{
+//                    {true ,false,false,false,true ,false,false},
+//                    {true ,false,false,true ,false,true ,true },
+//                    {false,true ,false,false,true ,false,false},
+//                    {false,true ,true ,false,false,false,true },
+//                    {true ,false,false,false,true ,true ,false},
+//                    {false,true ,false,false,true ,false,false},
+//                    {false,true ,true ,true ,true ,true ,false},
+//                    {false,false,false,true ,false,false,false}
+//            };
+//            boolean[][] hLines = new boolean[][]{
+//                    {false,false,true ,true ,false,false,true ,false},
+//                    {false,false,true ,true ,false,true ,false,false},
+//                    {true ,true ,false,true ,true ,false,true ,true },
+//                    {false,false,true ,false,true ,true ,false,false},
+//                    {false,true ,true ,true ,true ,false,true ,true },
+//                    {true ,false,false,true ,false,false,true ,false},
+//                    {false,true ,false,false,false,true ,false,true }
+//            };
+//            maze.setVerticalLines(vLines);
+//            maze.setHorizontalLines(hLines);
+//            maze.setStartPosition(0, 0);
+//            maze.setFinalPosition(7, 7);
+//        }
+//
+//        return maze;
     }
 
 }
